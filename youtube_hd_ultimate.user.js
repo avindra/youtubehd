16271257bdbcd46899f212185cb62b6f100353f5
Avindra Goolcharan <aavindraa@gmail.com>
2010-08-25 17:44:07 +0000

    Remove old, commented out hack. Used to be needed back in the chaotic days of YouTube.

--- youtube_hd_ultimate.user.js
+++ youtube_hd_ultimate.user.js
@@ -134,7 +134,6 @@ function fitBig(force) {
 	globals.setWidth(Math.round((player.offsetHeight - globals.getHeight()) * (config.IS_WIDESCREEN ? 1.77 : 1.32)));
 	center();
 }
-// if (player.PercentLoaded()!=100) player.src += "";
 GM_addStyle("#vidtools > * {\
 	position : relative;\
 	z-index : 6 !important;\
